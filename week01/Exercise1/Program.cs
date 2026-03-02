3045e535940eda2ac6df43fcb99e02e7e361c19d
bananajyncarloss <banana.jyncarloss@gmail.com>
2026-03-02 19:01:37 +0800

    Minor Changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
