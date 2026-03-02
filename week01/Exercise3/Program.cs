d967ffd4e72895e7894fdadffcc0074b17b035a1
bananajyncarloss <banana.jyncarloss@gmail.com>
2026-03-02 18:45:42 +0800

    Exercise 3 - Added how to know how many guesses it took

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int randomNumber = random.Next(1, 101);
+        int guess = -1;
+        int attempts = 0;
+
+        while (guess != randomNumber)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            attempts++;
+
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the number!");
+            }
+        }
+
+        Console.WriteLine($"Your random number is: {randomNumber}");
+        Console.WriteLine($"It took you {attempts} attempts to guess the number.");
     }
 }
