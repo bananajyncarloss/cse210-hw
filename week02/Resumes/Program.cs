faedfb715ca25bbfcf798f97bae887ab0d62fc5d
bananajyncarloss <banana.jyncarloss@gmail.com>
2026-03-09 21:03:47 +0800

    did step 7 and 8

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -17,6 +17,13 @@ class Program
         job2._company = "Apple";
         job2._startYear = "2022";
         job2._endYear = "2023";
-        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
 }
